1946a9950b0973894acfdeb6cd4e2ca59c8efc4f
hoohaha <hahaha1321@cau.ac.kr>
2021-05-23 23:53:56 +0900

    Make 8_array_class solution and complete array_sum project

---
+++ C&C++/8_array_class/8_array_class/array_sum.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//int 형에 원소의 갯수가 5개인 배열 생성
+	//생성 후 배열에 1, 3, 5, 7, 9 할당
+	//해당 배열의 원소 5개를 더할 것이다.
+	int arr[5] = {1, 3, 5, 7, 9};
+	
+	//int 형 변수 sum 을 생성하고 0을 할당한다.
+	//변수 sum 에 배열 arr[5] 속의 원소들을 더할 것이다.
+	int sum = 0;
+
+	// i = 0부터 i = 4까지 5번 반복되는 for문을 수행한다.
+	for (int i = 0; i < 5; i++)
+	{
+		//sum 에 arr[0], arr[1], ..., arr[4]를 더할 것이다.
+		sum += arr[i];
+	}
+	
+	//합을 출력한다
+	cout << "sum : " << sum;
+	
+}
