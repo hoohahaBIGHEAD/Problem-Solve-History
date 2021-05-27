a9f4efc85205a5d56b4039cb36b85c2ec4a71fe7
hoohaha <hahaha1321@cau.ac.kr>
2021-05-27 17:24:17 +0900

    Add comments

--- C&C++/13_matrix_calculator/13_matrix_calculator/main.cpp
+++ C&C++/13_matrix_calculator/13_matrix_calculator/main.cpp
@@ -2,28 +2,24 @@
 using namespace std;
 
 
-#define MAX_ROW_COL 3
+#define MAX_ROW_COL 3	//최대 행과 열의 갯수를 지정한다.
 
+//행렬 할당
 int A[MAX_ROW_COL][MAX_ROW_COL];
 int B[MAX_ROW_COL][MAX_ROW_COL];
 int C[MAX_ROW_COL][MAX_ROW_COL];
 
+//각 행렬의 행, 열을 저장한다. 이를 이용해 연산이 가능한지 판단한다.
 int A_row, A_col, B_row, B_col, C_row, C_col;
-
 
 void Run();
 
-
 int main() {
 
+	//행렬 연산 시행
 	Run();
 	return 0;
-
-
 }
-
-
-
 
 void Run() {
 	while (true)
@@ -45,21 +41,14 @@ void Run() {
 		A_col = col;
 
 		cout << "A(" << row << "X" << col << ") 행렬의 원소를 알려주세요." << endl;
-		//printf("ex) 2행 3열이면\n\"1 2 1\"\n\"3 1 2\" 입력\n2행 2열이면\n\"4 2\"\n\"1 2\"\n 입력\n");
 		for (size_t i = 0; i < row; i++)
 			for (size_t j = 0; j < col; j++)
 			{
+				//cin 입력은 엔터를 읽을 때까지 버퍼에 남아 있다.
 				cin >> row_temp[j];
+				//행렬의 i열들에 값을 입력한다.
 				A[i][j] = row_temp[j];
 			}
-
-		/*printf("입력한 행렬은 아래와 같습니다.\n");
-		for (size_t i = 0; i < row; i++)
-		{
-			for (size_t j = 0; j < col; j++)
-				cout << A[i][j] << " ";
-			cout << "\n";
-		}*/
 
 		cout << "B 행렬의 크기를 알려주세요." << endl;
 		printf("ex) 1X3 :: \"1 3\", 4X2 :: \"4 2\" 입력\n");
@@ -68,33 +57,31 @@ void Run() {
 		B_col = col;
 
 		cout << "B(" << row << "X" << col << ") 행렬의 원소를 알려주세요." << endl;
-		//printf("ex) 2행 3열이면\n\"1 2 1\"\n\"3 1 2\" 입력\n2행 2열이면\n\"4 2\"\n\"1 2\"\n 입력\n");
 		for (size_t i = 0; i < row; i++)
 			for (size_t j = 0; j < col; j++)
 			{
+				//cin 입력은 엔터를 읽을 때까지 버퍼에 남아 있다.
 				cin >> row_temp[j];
+				//행렬의 i열들에 값을 입력한다.
 				B[i][j] = row_temp[j];
 			}
 
-		/*printf("입력한 행렬은 아래와 같습니다.\n");
-		for (size_t i = 0; i < row; i++)
-		{
-			for (size_t j = 0; j < col; j++)
-				cout << B[i][j] << " ";
-			cout << "\n";
-		}*/
-
+		//주어진 행렬로 연산을 시행한다.
 		while (true)
 		{
 			printf("수행할 연산을 입력하세요. 1: 덧셈(A+B) 2: 뺄셈(A-B) 3: 곱셈(A*B) 4: 새로운 행렬 입력\n");
 			cin >> index;
+			//덧셈 수행
 			if (index == 1)
 			{
+				//A와 B가 같은 크기인지 확인
 				if (A_row == B_row && A_col == B_col)
 				{
+					//C의 행렬 크기를 지정한다.
 					C_row = A_row;
 					C_col = A_col;
 
+					//덧셈 연산 수행
 					for (size_t i = 0; i < A_row; i++)
 						for (size_t j = 0; j < A_col; j++)
 							C[i][j] = A[i][j] + B[i][j];
@@ -105,21 +92,25 @@ void Run() {
 					{
 						for (size_t j = 0; j < C_col; j++)
 							cout << C[i][j] << " ";
+						//행 출력 후 엔터
 						cout << "\n";
 					}
 				}
+				//A와 B가 같은 크기가 아니라면
 				else
-				{
 					printf("할 수 없습니다.\n");
-				}
 			}
+			//뺄셈 수행
 			else if (index == 2)
 			{
+				//A와 B가 같은 크기인지 확인
 				if (A_row == B_row && A_col == B_col)
 				{
+					//C의 행렬 크기를 지정한다.
 					C_row = A_row;
 					C_col = A_col;
 
+					//뺄셈 연산 수행
 					for (size_t i = 0; i < A_row; i++)
 						for (size_t j = 0; j < A_col; j++)
 							C[i][j] = A[i][j] - B[i][j];
@@ -130,22 +121,30 @@ void Run() {
 					{
 						for (size_t j = 0; j < C_col; j++)
 							cout << C[i][j] << " ";
+						//행 출력 후 엔터
 						cout << "\n";
 					}
 				}
+				//A와 B가 같은 크기가 아니라면
 				else
-				{
 					printf("할 수 없습니다.\n");
-				}
 			}
+			//곱셈 수행
 			else if (index == 3)
 			{
+				//A의 열과 B의 행 수가 일치하는지 확인
 				if (A_col == B_row)
 				{
+					//C의 행렬 크기를 지정한다.
 					C_row = A_row;
 					C_col = B_col;
 
-
+					/*for문 3번 돈다.
+					1, 2번째 for문 : C[i][j]를 구하기 위해 연산
+					3번째 for문 : k
+					C[i][j]는 시그마 A의 i행 k열 * B의 k행 j열이다.
+					이를 아래에 구현했다.
+					*/
 					for (size_t i = 0; i < A_row; i++)
 						for (size_t j = 0; j < B_col; j++)
 							for (size_t k = 0; k < A_row; k++)
@@ -157,14 +156,15 @@ void Run() {
 					{
 						for (size_t j = 0; j < C_col; j++)
 							cout << C[i][j] << " ";
+						//행 출력 후 엔터
 						cout << "\n";
 					}
 				}
+				//A의 열과 B의 행 수가 일치하지 않는다면
 				else
-				{
 					printf("할 수 없습니다.\n");
-				}
 			}
+			//행렬의 계산을 빠져나간다. 그러면 Run()의 첫번째 while 문으로 들어간다.
 			else if (index == 4)
 			{
 				printf("새로운 행렬을 입력합니다.\n");
@@ -172,14 +172,3 @@ void Run() {
 			}
 		}
 	}
-
-	
-
-	
-
-	
-
-
-
-
-}
