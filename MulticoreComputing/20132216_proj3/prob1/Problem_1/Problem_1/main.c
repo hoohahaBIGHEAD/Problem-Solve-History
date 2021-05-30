630785d64d9d11b840346cc0a08872593f4484e7
hoohaha <hahaha1321@cau.ac.kr>
2021-05-30 23:09:24 +0900

    Make projects

--- MulticoreComputing/20132216_proj3/prob1/Problem_1/Problem_1/main.c
+++ MulticoreComputing/20132216_proj3/prob1/Problem_1/Problem_1/main.c
@@ -1,3 +1,7 @@
+#include <omp.h>
+#include <stdio.h>
+#include<stdlib.h>
+
 typedef enum _boolean {
 	FALSE,
 	TRUE
@@ -6,9 +10,58 @@ typedef enum _boolean {
 #define FALSE 0
 #define TRUE 1
 
+#define PRIMENUMBER 200000
+
 int main(int argc, char* argv[]) {
+	int count = 0;
+	int type, threadNumber;
+	double t1, t2;
+	int i;
+	type = atoi(argv[1]);
+	threadNumber = atoi(argv[2]);
+	omp_set_num_threads(threadNumber);
+	printf("mode : %d\t threads : %d\n", type, threadNumber);
+	
+	t1 = omp_get_wtime();
+	if (type == 1)
+	{
+#pragma omp parallel for schedule(static) reduction(+:count)
+		for (i = 0; i < PRIMENUMBER; i++) {
+			int val = 0;
+			if (isPrime(i))
+				val = val + 1;
+			count = count + val;
+		}
+
+	}
+	else if (type == 2)
+	{
+#pragma omp parallel for schedule(dynamic) reduction(+:count)
+		for (i = 0; i < PRIMENUMBER; i++) {
+			int val = 0;
+			if (isPrime(i))
+				val = val + 1;
+			count = count + val;
+		}
+	}
+	else if (type == 3)
+	{
+#pragma omp parallel for schedule(guided) reduction(+:count)
+		for (i = 0; i < PRIMENUMBER; i++) {
+			int val = 0;
+			if (isPrime(i))
+				val = val + 1;
+			count = count + val;
+		}
+	}
+	else
+	{
+		printf("잘못된 입력입니다.\n");
+	}
+	t2 = omp_get_wtime();
 
 
+	printf("count : %d\tcomputation time : %lf\n", count, t2-t1);
 	return 0;
 }
 
