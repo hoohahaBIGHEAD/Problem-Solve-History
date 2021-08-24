546fe5e0431c1a2c164d431c6796efed7a0d487d
hoohaha <hahaha1321@cau.ac.kr>
2021-08-24 18:05:28 +0900

    get data from upbit

--- Python/AutoTrading/ch03/03_14.py
+++ Python/AutoTrading/ch03/03_14.py
@@ -1,27 +1,47 @@
 # ch03/03_14.py
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
+from PyQt5 import uic
+from PyQt5.QtCore import *
+import pyupbit
+
+"""
 
 
-class MyWindow(QMainWindow):
+upbit = pyupbit.Upbit(access_key,secret_key)
+print(upbit.get_balances())
+
+#ret = upbit.cancel_order('aa4642f5-6fb1-4251-a0ae-64035f31e86d')
+#print(ret)
+
+
+#df = pyupbit.get_ohlcv("KRW-ETH", count=5)
+#print(df)
+
+#orderbook = pyupbit.get_orderbook("KRW-ETH")
+bids_asks = orderbook[0]['orderbook_units']
+for bid_ask in bids_asks:
+    print(bid_ask)
+"""
+
+form_class = uic.loadUiType("uiMyWindow.ui")[0]
+
+
+class MyWindow(QMainWindow, form_class):
     def __init__(self):
         super().__init__()
-        # 위젯 생성 코드
-        self.setGeometry(100, 200, 300, 400)
-        self.setWindowTitle("PyQt")
-        self.setWindowIcon(QIcon("dog.png"))
+        self.setupUi(self)
 
-        btn1 = QPushButton("버튼1", self)
-        btn1.move(10, 10)
-        btn1.clicked.connect(self.btn_clicked)
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.inquiry)
 
-        btn2 = QPushButton("버튼2", self)
-        btn2.move(10, 40)
-
-    # 이벤트 처리 코드
-    def btn_clicked(self):
-        print("버튼 클릭")
+    def inquiry(self):
+        cur_time = QTime.currentTime()
+        str_time = cur_time.toString("hh:mm:ss")
+        self.statusBar().showMessage(str_time)
+        price = pyupbit.get_current_price(["KRW-ETH"])['KRW-ETH']
+        self.lineEdit.setText(str(price))
 
 
 # QApplication 객체 생성 및 이벤트 루프 생성 코드
