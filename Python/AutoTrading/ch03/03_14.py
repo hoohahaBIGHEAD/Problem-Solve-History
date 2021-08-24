0eee054c3ab3a2b6c439f2cd62bdaf07ad8e5949
hoohaha <hahaha1321@cau.ac.kr>
2021-08-24 17:06:08 +0900

    ui by coding

--- Python/AutoTrading/ch03/03_14.py
+++ Python/AutoTrading/ch03/03_14.py
@@ -1,14 +1,31 @@
 # ch03/03_14.py
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
 
-class Mywindow(QMainWindow):
+
+class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(100,200,300,400)
+        # 위젯 생성 코드
+        self.setGeometry(100, 200, 300, 400)
         self.setWindowTitle("PyQt")
+        self.setWindowIcon(QIcon("dog.png"))
 
+        btn1 = QPushButton("버튼1", self)
+        btn1.move(10, 10)
+        btn1.clicked.connect(self.btn_clicked)
+
+        btn2 = QPushButton("버튼2", self)
+        btn2.move(10, 40)
+
+    # 이벤트 처리 코드
+    def btn_clicked(self):
+        print("버튼 클릭")
+
+
+# QApplication 객체 생성 및 이벤트 루프 생성 코드
 app = QApplication(sys.argv)
-window = Mywindow()
+window = MyWindow()
 window.show()
 app.exec_()
